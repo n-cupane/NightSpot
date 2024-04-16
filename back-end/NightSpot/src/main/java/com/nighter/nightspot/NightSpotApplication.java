31d2197c3dc9e332961f4ffe88266cddd95a4537
n-cupane <92432010+n-cupane@users.noreply.github.com>
2024-04-16 17:03:40 +0200

    project init

---
+++ back-end/NightSpot/src/main/java/com/nighter/nightspot/NightSpotApplication.java
@@ -0,0 +1,13 @@
+package com.nighter.nightspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightSpotApplication.class, args);
+	}
+
+}
---
+++ back-end/NightSpot/src/test/java/com/nighter/nightspot/NightSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nighter.nightspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
