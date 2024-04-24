9d260cface9cefd0d83efd5d789216ec75792fce
AndreaRandazzo <153932512+AndreaRandazzo@users.noreply.github.com>
2024-04-24 10:36:49 +0200

    Update SpotFragment.java

--- android/NightSpot/app/src/main/java/com/nighter/nightspot/fragments/SpotFragment.java
+++ android/NightSpot/app/src/main/java/com/nighter/nightspot/fragments/SpotFragment.java
@@ -2,6 +2,8 @@ package com.nighter.nightspot.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,20 +11,32 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.nighter.nightspot.R;
+import com.nighter.nightspot.databinding.FragmentSpotBinding;
 
 
 public class SpotFragment extends Fragment {
 
+    private SpotFragmentArgs args;
+    private FragmentSpotBinding binding;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        if(getArguments()!=null) {
+            args = SpotFragmentArgs.fromBundle(getArguments());
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_spot, container, false);
+        binding = FragmentSpotBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
